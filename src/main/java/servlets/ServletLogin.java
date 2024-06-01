3f8367d07f4bf9c38905638c59004568c093b73f
Seu Nome <seuemail@example.com>
2024-05-31 21:34:47 -0400

    criação do projeto

---
+++ src/main/java/servlets/ServletLogin.java
@@ -0,0 +1,82 @@
+package servlets;
+
+import java.io.IOException;
+
+import dao.DAOLoginRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.ModelLogin;
+
+
+/*O chamando Controller s�o as servlets ou ServletLoginController*/
+@SuppressWarnings("hiding")
+@WebServlet(urlPatterns = {"/principal/ServletLogin", "/ServletLogin"}) /*Mapeamento de URL que vem da tela*/
+public class ServletLogin<RequestDispatcher> extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private DAOLoginRepository daoLoginRepository = new DAOLoginRepository();
+
+  
+    public ServletLogin() {
+    }
+
+
+    /*Recebe os dados pela url em parametros*/
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	
+	/*recebe os dados enviados por um formulario*/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		String url = request.getParameter("url");
+		
+		try {
+		
+				if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+					
+					ModelLogin modelLogin = new ModelLogin();
+					modelLogin.setLogin(login);
+					modelLogin.setSenha(senha);
+					
+					if (daoLoginRepository.validarAutenticacao(modelLogin)) { /*Simulando login*/
+						
+						request.getSession().setAttribute("usuario", modelLogin.getLogin());
+						
+						if (url == null || url.equals("null")) {
+							url = "principal/principal.jsp";
+						}
+						
+						jakarta.servlet.RequestDispatcher redirecionar = request.getRequestDispatcher(url);
+						redirecionar.forward(request, response);
+						
+					}else {
+						jakarta.servlet.RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
+						request.setAttribute("msg", "Informe o login e senha corretamente!");
+						redirecionar.forward(request, response);
+					}
+					
+				}else {
+					jakarta.servlet.RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+					request.setAttribute("msg", "Informe o login e senha corretamente!");
+					redirecionar.forward(request, response);
+				}
+		
+		}catch (Exception e) {
+			e.printStackTrace();
+			jakarta.servlet.RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+		}
+		
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="ISO-8859-1">
+	<title>Curso JSP</title>
+</head>
+<body>
+
+<h1>Bem vindo ao curso de JSP</h1>  
+
+
+<form action="ServletLogin" method="post">
+<input type="hidden" value="<%= request.getParameter("url") %>" name="url">
+<table>
+ <tr>
+  <td><label>Login</label> </td>
+  <td><input name="login" type="text"></td>
+ </tr>
+ 
+ <tr>
+  <td><label>Senha</label> </td>
+  <td>
+  <input name="senha" type="password">
+  </td>
+ </tr>
+ 
+ <tr>
+  <td/>
+  <td>
+   <input type="submit" value="Enviar">
+  </td>
+ </tr>
+</table>
+
+</form>
+
+<h4>${msg}</h4>
+
+</body>
+</html>
